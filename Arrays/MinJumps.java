667febd2db019ad821a8ba4820c20a234fa55432
Alisha07 <alisha.02929@gmail.com>
2022-04-02 17:21:27 +0530

    Linear time approach to solve jump game problem

---
+++ Arrays/MinJumps.java
@@ -0,0 +1,40 @@
+public class MinJumps {
+
+    private static int Jump(int[] num) {
+        int a = num[0];
+        int b = num[0];
+        int jump = 1;
+        if (a == 0) {
+            return -1;
+        }
+        for (int i = 1; i < num.length; i++) {
+            if (i == num.length - 1) {
+                return jump;
+            }
+            a--;
+            b--;
+
+            if (num[i] > b) {
+                b = num[i];
+            }
+
+            if (num[i] == 0) {
+                return -1;
+            }
+
+            if (a == 0) {
+                jump++;
+                a = b;
+            }
+
+        }
+        return jump;
+    }
+
+    public static void main(String[] args) {
+        int num[] = { 3, 2, 1, 0, 4 };
+
+        System.out.println("Minimum number of jumps to reach end is : "
+                + Jump(num));
+    }
+}
