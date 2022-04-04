2a4ac4f9a406c7f138314a32ffa991a7a4a19da0
Alisha07 <alisha.02929@gmail.com>
2022-04-04 16:46:03 +0530

    Reverse Array using 2-pointer method

---
+++ CommClassroom/revArray.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class revArray {
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 23, 9, 18, 56 };
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+
+    }
+
+    private static void swap(int[] arr, int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+
+}
