c43039a867bd51f980da1a4c87f16f15bba38ff0
Alisha07 <alisha.02929@gmail.com>
2022-04-05 20:34:00 +0530

    Number of Good Pairs

---
+++ CommClassroom/GoodPairs.java
@@ -0,0 +1,20 @@
+public class GoodPairs {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 1, 1, 3 };
+        System.out.println(numIdenticalPairs(nums));
+    }
+
+    private static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+}
