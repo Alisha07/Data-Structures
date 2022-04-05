800d82e5dfccb71175508d3009e91ed006aa8e19
Alisha07 <alisha.02929@gmail.com>
2022-04-05 11:20:34 +0530

    Shuffle the Array

---
+++ CommClassroom/Shuffle.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Shuffle {
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 5, 1, 3, 4, 7 };
+        int n = 3;
+        System.out.println(Arrays.toString(shuffle(nums, n)));
+    }
+
+    public static int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < n; i++) {
+            ans[i * 2] = nums[i];
+            ans[i * 2 + 1] = nums[n + i];
+        }
+        return ans;
+    }
+
+}
