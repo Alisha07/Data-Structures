e08f941d6e9d32844058864979742cc430782a06
Alisha07 <alisha.02929@gmail.com>
2022-04-06 22:13:03 +0530

    Floor of a number in a sorted array

---
+++ CommClassroom/Floor.java
@@ -0,0 +1,31 @@
+public class Floor {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 5, 9, 14, 16, 18 };
+        int target = 10;
+        System.out.println(floor(arr, target));
+    }
+
+    private static int floor(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        // binary(arr, target, start, end);
+        if (target > arr[end]) {
+            return -1;
+        }
+        while (start <= end) {
+            int mid = start + ((end - start) / 2);
+            if (target == arr[mid]) {
+                return arr[mid];
+            }
+            if (target < arr[mid]) {
+                end = mid - 1;
+            }
+            if (target > arr[mid]) {
+                start = mid + 1;
+            }
+
+        }
+        return arr[end];
+
+    }
+}
