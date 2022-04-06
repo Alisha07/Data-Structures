399cac74e988e5479793d112eeee6a1ca486d64b
Alisha07 <alisha.02929@gmail.com>
2022-04-07 00:37:00 +0530

    Two Sum

---
+++ CommClassroom/twoSum.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class twoSum {
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum1(nums, target)));
+    }
+
+    public static int[] twoSum1(int[] nums, int target) {
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+        return ans;
+    }
+}
